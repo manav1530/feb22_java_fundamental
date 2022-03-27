5a944755552d8fd296e03013b787e37823b5ebb7
manav1530 <68682908+manav1530@users.noreply.github.com>
2022-03-27 17:54:45 +0530

    Add files via upload

---
+++ february/week1/week2/Bitwise.java
@@ -0,0 +1,15 @@
+package assignment_feb22;
+
+public class Bitwise {
+
+	public static void main(String[] args) {
+		 int a = 7 , b = 5;
+		 System.out.println(a&b);
+		 int c =17,d = 9;
+		 System.out.println(c|d);
+		 int e = 91 , f = 56;
+		 System.out.println(e^f);
+
+	}
+
+}
---
+++ february/week1/week2/Formula2.java
@@ -0,0 +1,26 @@
+package assignment_feb22;
+
+public class Formula2 {
+
+	public static void main(String[] args) {
+		int count = 0, i=0	;
+		 while (count<7)
+		 {
+			 int num = 	i+6*i;
+			 i++;
+			 if(num % 6==0)
+			 {
+				 System.out.println(num);
+				 count++;
+				 
+			 }
+			 
+
+		}
+
+	}
+	
+
+	}
+
+
